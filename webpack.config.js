3d7893f3db4daaf3fb6b011313f1861224d2eef1
Vladyslav <@gmail.com>
2019-09-18 15:03:13 +0300

    Sass, css loaders & postcss plagins - autoprefixer css-mqpacker cssnano

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
@@ -10,15 +11,60 @@ module.exports = {
     publicPath: '/dist'
   },
   module: {
-      rules: [{
-          test: /\.js/,
+      rules: [
+        {
+          test: /\.js$/,
           loader: 'babel-loader',
           exclude: '/node_modules/'
-      }]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true}
+            },
+            {
+              loader: 'postcss-loader',
+              options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' }}
+            },
+          ]
+        },
+        {
+          test: /\.sass$/,
+          use: [
+            'style-loader',
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true}
+            },
+            {
+              loader: 'postcss-loader',
+              options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' }}
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true}
+            }
+          ]
+        }
+      ]
   },
   devServer: {
     overlay: true
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      // chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ]
 };
 
 // path: path.resolve(__dirname, 'dist'),
